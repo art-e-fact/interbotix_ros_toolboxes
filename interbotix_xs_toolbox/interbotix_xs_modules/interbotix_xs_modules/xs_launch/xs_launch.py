c3e1ca968f945239e2fa2993a0ea19e734e32bd4
Bach Nguyen <ngb1998@gmail.com>
2025-06-09 21:46:22 +0900

    migrate TrossenRoboticsOfficeBuilding world -> fix dart::dynamics::BoxShape assertion error

--- interbotix_xs_toolbox/interbotix_xs_modules/interbotix_xs_modules/xs_launch/xs_launch.py
+++ interbotix_xs_toolbox/interbotix_xs_modules/interbotix_xs_modules/xs_launch/xs_launch.py
@@ -556,7 +556,7 @@ def determine_use_sim_time_param(
     :param hardware_type: The `hardware_type` LaunchConfiguration
     :return: True if hardware is simulated, the `use_sim_time` LaunchConfiguration otherwise
     """
-    if hardware_type_launch_arg.perform(context) in ('gz_classic'):
+    if hardware_type_launch_arg.perform(context) in ('gz_sim'):
         return TextSubstitution(text='true')
     else:
         return LaunchConfiguration('use_sim_time')
